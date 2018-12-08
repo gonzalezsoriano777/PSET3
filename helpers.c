0eb9651980fd3a805469a23800dee6602e15e6a3
Hector Gonzalez <raftb.gonzalez@outlook.com>
2018-12-08 16:39:19 +0000

    Lets work

---
+++ helpers.c
@@ -0,0 +1,41 @@
+// Helper functions for music
+
+#include <cs50.h>
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+
+#include "helpers.h"
+
+// Converts a fraction formatted as X/Y to eighths
+int duration(string fraction)
+{
+    // if(fraction[0] == '1')
+    // {
+    //     return 1;
+    // }
+    // if(fraction[1] == '8')
+    // {
+    //     return 8;
+    // }
+    // if(fraction[2] == '4')
+    // {
+    //     return 4;
+    // }
+    // if(fraction[3] == '2')
+    // {
+    //     return 2;
+    // }
+}
+
+// Calculates frequency (in Hz) of a note
+int frequency(string note)
+{
+    // TODO
+}
+
+// Determines whether a string represents a rest
+bool is_rest(string s)
+{
+     if()
+}
