0ae2b1a7d14322ec05c8d19291c3562a0e81d824
Hector Gonzalez <raftb.gonzalez@outlook.com>
2018-12-09 20:24:09 +0000

    Success

--- helpers.c
+++ helpers.c
@@ -11,24 +11,80 @@
 // Converts a fraction formatted as X/Y to eighths
 int duration(string fraction)
 {
-
+    int xword = atoi(&fraction[0]);
+    int yword = atoi(&fraction[2]);
+    int Dur = (8. / yword) * xword;
+    return Dur;
 }
 
 // Calculates frequency (in Hz) of a note
 int frequency(string note)
 {
-    // TODO
+    int oct; // Octave
+    int n = 0; // How the note will start from
+    if (strlen(note) == 3)
+    {
+        oct = atoi(&note[2]); // Formula for Octave
+        char accident = note[1]; // each character and value of the note
+        if (accident == '#')
+        {
+            n += 1;
+        }
+        else if (accident == 'b')
+        {
+            n -= 1;
+        }
+    }
+    else
+    {
+        oct = atoi(&note[1]);
+    }
+    char letter = note[0];
+
+    if (letter == 'A')
+    {
+        n += 0;
+    }
+    else if (letter == 'B')
+    {
+        n += 2;
+    }
+    else if (letter == 'C')
+    {
+        n -= 9;
+    }
+    else if (letter == 'D')
+    {
+        n -= 7;
+    }
+    else if (letter == 'E')
+    {
+        n -= 5;
+    }
+    else if (letter == 'F')
+    {
+        n -= 4;
+    }
+    else if (letter == 'G')
+    {
+        n -= 2;
+    }
+    n += (oct - 4) * 12;
+
+    float power = n / 12.;
+    float f = round(pow(2, power) * 440);
+    return f;
 }
 
 // Determines whether a string represents a rest
 bool is_rest(string s)
 {
-     if(strlenl(s) == 0)
-     {
-         return true;
-     }
-     else
-     {
-         return false;
-     }
+    if (strlen(s) == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
