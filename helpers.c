c2fc68737c3d1d00677b7a57d4bc4f324d6d5878
Hector Gonzalez <raftb.gonzalez@outlook.com>
2018-12-11 18:12:25 +0000

    Success

--- helpers.c
+++ helpers.c
@@ -22,59 +22,55 @@ int frequency(string note)
 {
     int oct; // Octave
     int n = 0; // How the note will start from
-    if (strlen(note) == 3)
+    if (strlen(note) == '3')
     {
-        oct = atoi(&note[2]); // Formula for Octave
-        char accident = note[1]; // each character and value of the note
-        if (accident == '#')
-        {
+        oct = atoi(&note[2]);
+        char accident = note[1];
+        if (accident=='#'){
             n += 1;
-        }
-        else if (accident == 'b')
-        {
+        }else if (accident == 'b'){
             n -= 1;
         }
     }
     else
     {
-        oct = atoi(&note[1]);
+        oct = atoi(&note[1]); // Octave formula
     }
     char letter = note[0];
 
-    if (letter == 'A')
-    {
+    if (letter=='A'){
         n += 0;
     }
-    else if (letter == 'B')
+    else if (letter=='B')
     {
         n += 2;
     }
-    else if (letter == 'C')
+    else if (letter=='C')
     {
         n -= 9;
     }
-    else if (letter == 'D')
+    else if (letter=='D')
     {
         n -= 7;
     }
-    else if (letter == 'E')
+    else if (letter=='E')
     {
         n -= 5;
     }
-    else if (letter == 'F')
+    else if (letter=='F')
     {
         n -= 4;
     }
-    else if (letter == 'G')
+    else if (letter=='G')
     {
         n -= 2;
     }
-    n += (oct - 4) * 12;
+    n += (oct-4) * 12;
 
-    float power = n / 12.;
-    float f = round(pow(2, power) * 440);
+    float power = n/12.;
+    float f = round(pow(2, power)*440);
     return f;
-}
+    }
 
 // Determines whether a string represents a rest
 bool is_rest(string s)
